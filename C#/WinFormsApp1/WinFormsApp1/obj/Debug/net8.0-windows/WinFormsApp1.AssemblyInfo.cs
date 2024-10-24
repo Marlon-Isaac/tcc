7720d1fbbe30311c260fa623d1f7ba48f46d95ce
Mauricio de Macedo <mauriciocomputer27@gmail.com>
2024-10-24 10:50:54 -0300

    a

--- C#/WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ C#/WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a5f34ad7362d025ec3e2d58e8569a02f6400dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51928e8e0fd6a78f08e963ad7852d95ceaedba8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
